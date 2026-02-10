69427b3ded5dbe49d00fc091fb86b30b9e0dfd4c
“mars” <belleqr@gmail.com>
2026-02-10 14:03:43 -0300

    first commit

---
+++ aula/tipos-de-dados.js
@@ -0,0 +1,70 @@
+// String (Textos)
+
+// 1
+const nome = "Clint";
+console.log(nome);
+
+// 2
+const sobrenome = "Barton";
+console.log(nome + " " + sobrenome);
+
+// 3
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+// 4
+const escola = "SENAI Guarulhos";
+console.log(escola);
+
+// 5
+const parte1 = "Eu estudo na";
+const parte2 = " área de programação.";
+console.log(parte1 + parte2);
+
+// 6
+console.log(`Seja bem-vindo, ${nome}!`);
+
+// 7
+console.log(nome.length);
+
+// 8
+console.log(nome.toUpperCase());
+
+// 9
+console.log(nome.toLowerCase());
+
+// 10
+const cidade = "Brooklyn";
+console.log(`Meu nome é ${nome} e moro em ${cidade}`);
+
+
+// Number (Números)
+
+// 1
+const num1 = 35;
+const num2 = 5;
+console.log(num1 + num2);
+
+// 2
+console.log(num1 - num2);
+
+// 3
+console.log(num1 * num2);
+
+// 4
+console.log(num1 / num2);
+
+
+
+// Boolean (Verdadeiro ou Falso)
+
+// 1
+const ativo = true;
+console.log(ativo);
+
+// 2
+const numero = 15;
+console.log(numero > 10);
+
+// 3
+const idade = 11;
+console.log(idade >= 18);
